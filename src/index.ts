e29c2fead519d11cf979cd54dc06c3295b74497a
Hadrian Hughes <hadrianhughes@gmail.com>
2022-10-07 10:59:54 +0100

    Translate the unit square

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ const CANVAS_WIDTH = 800
 const CANVAS_HEIGHT = 600
 const SCREEN_INCREMENT = 8
 
+const gridWidth = CANVAS_WIDTH / SCREEN_INCREMENT
+const gridHeight = CANVAS_HEIGHT / SCREEN_INCREMENT
+
 canvas.width = CANVAS_WIDTH
 canvas.height = CANVAS_HEIGHT
 
@@ -15,10 +18,13 @@ const unitSquare: Vector2D[] = [[0,0], [0,1], [1,0], [1,1]]
 
 const scale = ([a, b, c, d]: Matrix2x2) => ([x, y]: Vector2D): Vector2D => [a*x + b*y, c*x + d*y]
 
+const translate = ([tx, ty]: Vector2D) => ([x, y]: Vector2D): Vector2D => [x + tx, y + ty]
+
 const planeToScreen = ([gx, gy]: Vector2D): Vector2D => [gx * SCREEN_INCREMENT, CANVAS_HEIGHT - gy * SCREEN_INCREMENT]
 
 const vertices = unitSquare
   .map(scale([10, 0, 0, 10]))
+  .map(translate([gridWidth / 2 - 5, gridHeight / 2 - 5]))
   .map(planeToScreen)
 
 ctx.fillStyle = 'blue'
