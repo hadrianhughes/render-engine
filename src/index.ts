be054857c339edb1b306c79b7c8deb2d88edfbc2
Hadrian Hughes <hadrianhughes@gmail.com>
2022-10-07 10:22:52 +0100

    Set up canvas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('ran')
+const canvas: HTMLCanvasElement = document.getElementById('root') as HTMLCanvasElement
+const ctx = canvas.getContext('2d')
+
+canvas.width = 800
+canvas.height = 600
