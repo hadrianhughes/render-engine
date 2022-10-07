4a184d47fc1963b4814dc81678d11cfaafc200bb
Hadrian Hughes <hadrianhughes@gmail.com>
2022-10-07 11:04:48 +0100

    Rotate the unit square

--- src/index.ts
+++ src/index.ts
@@ -14,18 +14,25 @@ canvas.height = CANVAS_HEIGHT
 type Vector2D = [number, number]
 type Matrix2x2 = [number, number, number, number]
 
-const unitSquare: Vector2D[] = [[0,0], [0,1], [1,0], [1,1]]
+const unitSquare: Vector2D[] = [[0,0], [0,1], [1,1], [1,0]]
 
 const scale = ([a, b, c, d]: Matrix2x2) => ([x, y]: Vector2D): Vector2D => [a*x + b*y, c*x + d*y]
 
 const translate = ([tx, ty]: Vector2D) => ([x, y]: Vector2D): Vector2D => [x + tx, y + ty]
 
+const rotate = (rads: number) => ([x, y]: Vector2D): Vector2D => [x*Math.cos(rads) - y*Math.sin(rads), x*Math.sin(rads) + y*Math.cos(rads)]
+
 const planeToScreen = ([gx, gy]: Vector2D): Vector2D => [gx * SCREEN_INCREMENT, CANVAS_HEIGHT - gy * SCREEN_INCREMENT]
 
 const vertices = unitSquare
   .map(scale([10, 0, 0, 10]))
+  .map(rotate(Math.PI / 6))
   .map(translate([gridWidth / 2 - 5, gridHeight / 2 - 5]))
   .map(planeToScreen)
 
 ctx.fillStyle = 'blue'
-ctx.fillRect(vertices[0][0], vertices[2][1], vertices[3][0] - vertices[1][0], vertices[1][1] - vertices[0][1])
+ctx.beginPath()
+ctx.moveTo(...vertices[0])
+vertices.slice(1).forEach(([x, y]) => ctx.lineTo(x, y))
+ctx.closePath()
+ctx.fill()
