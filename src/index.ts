2db599f765aa06012e266288de148c9143979bfa
Hadrian Hughes <hadrianhughes@gmail.com>
2022-10-08 11:41:42 +0100

    Remove triangle polygon

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Polygon, unitSquare, scale, rotateZ, translate, planeToScreen, compose } from './math'
+import { unitSquare, scale, rotateZ, planeToScreen, compose } from './math'
 import { renderPolygon } from './render'
 
 const transform = compose(
@@ -10,10 +10,3 @@ const transform = compose(
 const vertices = unitSquare.map(transform)
 
 renderPolygon(vertices)
-
-const odd: Polygon = [[0,0,1], [0,1,1], [1,2,1]]
-renderPolygon(
-  odd.map(
-    compose(scale(10, 10, 1), translate([-20, -20, 0]), planeToScreen)
-  )
-)
